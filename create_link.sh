b1c9bf051524c6e5566b5d957b9c504140a19162
Fabio Alicante <alicantef@gmail.com>
2024-07-03 15:59:39 +0200

    add create link script

---
+++ create_link.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#usage: source create_link_miccern.sh run brick platelast platefirst 
+#always check for rescans
+
+RUN=$1
+BRICKID=$2
+NWALL=(${BRICKID: -2:1})
+NBRICK=(${BRICKID: -1:1})
+BRICKFOLDER="$(printf "b%0*d" 6 $BRICKID)"
+RAWDATAPATH=/eos/experiment/sndlhc/emulsionData/2022/Napoli/SND/mic2/RUN$RUN\_W$NWALL\_B$NBRICK 
+RECODATAPATH=/eos/experiment/sndlhc/emulsionData/2022/emureco_Napoli/RUN$RUN/$BRICKFOLDER
+
+for iplate in $(seq $4 $3)
+  do
+
+  RAWPLATEFOLDER="$(printf "P%02d" $(( 10#$iplate )))"
+  # RAWPLATEFOLDER="$(printf "P%02d_RESCAN" $(( 10#$iplate )))"
+  PLATEFOLDER="$(printf "p%03d" $(( 10#$iplate )))"
+  mkdir $RECODATAPATH/$PLATEFOLDER
+  ln -s -f $RAWDATAPATH/$RAWPLATEFOLDER/tracks.raw.root $RECODATAPATH/$PLATEFOLDER/$BRICKID.$iplate.0.0.raw.root
+  echo created link for $PLATEFOLDER to $RAWDATAPATH/$RAWPLATEFOLDER
+
+  done
