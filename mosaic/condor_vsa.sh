c5cd7df8a05e0d2f6fffd2c145af654a4a69975c
Fabio Alicante <alicantef@gmail.com>
2024-07-11 16:28:54 +0200

    add mosalignbeam and correct condor path

--- mosaic/condor_vsa.sh
+++ mosaic/condor_vsa.sh
@@ -6,11 +6,11 @@ BRICKFOLDER="$(printf "b%0*d" 6 $BRICKID)"
 PLATENUMBER=$3
 
 echo "Set up SND environment"
-SNDBUILD_DIR=/afs/cern.ch/work/s/snd2na/public/SNDBUILD/sw
-#source /cvmfs/sndlhc.cern.ch/SNDLHC-2023/Aug30/setUp.sh
-#eval `alienv load -w $SNDBUILD_DIR --no-refresh sndsw/latest`
-#lates fedra not yet released
-source /eos/user/s/snd2na/sw/fedra/setup_new.sh	
+SNDBUILD_DIR=/afs/cern.ch/user/s/snd2cern/public/SNDBUILD/sw
+source /cvmfs/sndlhc.cern.ch/SNDLHC-2023/Aug30/setUp.sh
+eval `alienv load -w $SNDBUILD_DIR --no-refresh sndsw/latest`
+echo "Loading FEDRA"
+source /afs/cern.ch/user/s/snd2cern/public/fedra/setup_new.sh
 
 echo  "go into reconstruction folder "
 cd /eos/experiment/sndlhc/emulsionData/2022/emureco_Napoli/RUN$RUN/$BRICKFOLDER
