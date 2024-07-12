258b3bd9c9c6739658f92188a4a9b9d24ed89f92
Fabio Alicante <alicantef@gmail.com>
2024-07-12 10:17:06 +0200

    add shrinkage and moslink

---
+++ linking/condor_mln.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+RUN= #put run number
+BRICKID= #put brick id
+BRICKFOLDER="$(printf "b%0*d" 6 $BRICKID)"
+PLATENUMBER=$3
+
+echo "Set up SND environment"
+SNDBUILD_DIR=/afs/cern.ch/work/s/snd2na/public/SNDBUILD/sw
+source /cvmfs/sndlhc.cern.ch/SNDLHC-2023/Aug30/setUp.sh
+eval `alienv load -w $SNDBUILD_DIR --no-refresh sndsw/latest`
+echo "Loading FEDRA"
+source /afs/cern.ch/work/s/snd2na/public/fedra/setup_new.sh	
+
+echo  "go into reconstruction folder "
+cd /eos/experiment/sndlhc/emulsionData/2022/emureco_Napoli/RUN$RUN/$BRICKFOLDER
+
+echo "moslink $BRICKID.$PLATENUMBER.0.0"
+source moslink.sh $BRICKID $PLATENUMBER
+
