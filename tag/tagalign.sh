899c4d579fccce8a7743fb649f05dfa6344acfec
Fabio Alicante <alicantef@gmail.com>
2024-07-23 12:01:47 +0200

    separate vsa and tag

--- tag/tagalign.sh
+++ tag/tagalign.sh
@@ -1,4 +1,3 @@
 BRICKID=$1
 i=$2
-
 tagalign -A=$BRICKID.$((i+1)).0.0 -B=$BRICKID.$((i)).0.0 -new -v=2
--- tag/tagtra.sh
+++ tag/tagtra.sh
@@ -1,5 +1,4 @@
 BRICKID=$1
-
 mkdir out_tag
 seq 1 56 | xargs -i -P 15 bash -c 'echo tagalign '$BRICKID'.{}.0.0 && . ./tagalign.sh '$BRICKID' {} > out_tag/'$BRICKID'.{}.0.0.tal.txt'
 makescanset -set=$BRICKID.100.0.0 -dz=-1350 -from_plate=57 -to_plate=1 -dzbase=195
