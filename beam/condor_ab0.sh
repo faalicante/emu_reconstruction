da7de3b941f3ceed3260b62650262cb56967eccf
Fabio Alicante <alicantef@gmail.com>
2024-08-13 10:18:56 +0200

    add ab0

---
+++ beam/condor_ab0.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+RUN= #put run number
+BRICKID= #put brick id
+BRICKFOLDER="$(printf "b%0*d" 6 $BRICKID)"
+PLATENUMBER=$3
+
+echo "Set up SND environment"
+SNDBUILD_DIR=/afs/cern.ch/user/s/snd2cern/public/SNDBUILD/sw
+source /cvmfs/sndlhc.cern.ch/SNDLHC-2023/Aug30/setUp.sh
+eval `alienv load -w $SNDBUILD_DIR --no-refresh sndsw/latest`
+source /afs/cern.ch/user/s/snd2cern/public/fedra/setup_new.sh	
+
+echo  "go into reconstruction folder "
+cd /eos/experiment/sndlhc/emulsionData/2022/emureco_CERN/RUN$RUN/$BRICKFOLDER
+
+echo "viewsideal $BRICKID.$PLATENUMBER.0.0"
+source viewsideal.sh $BRICKID $PLATENUMBER
+
+echo "mosalignbeam $BRICKID.$PLATENUMBER.0.0"
+source mosalignbeam.sh $BRICKID $PLATENUMBER
+
