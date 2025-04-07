264c9da8e338a6fb1eaa83aa57e0fa2696212566
Fabio <alicantef@gmail.com>
2025-04-07 17:19:01 +0200

    align set with AFF.tasr

---
+++ 4.align/align_set.sh
@@ -0,0 +1,2 @@
+BRICKID=$1
+emalign -set=$BRICKID.0.0.0 -new -v=2
--- 4.align/condor_al.sh
+++ 4.align/condor_al.sh
@@ -7,13 +7,9 @@ BRICKID=$3
 BRICKFOLDER=$4
 CELL=$5
 CELLFOLDER=$6
-PLATENUMBER=$7
-PLATEFOLDER=$8
 xcell=$((CELL % 18 + 1))
 ycell=$((CELL / 18 + 1))
-PLATENEXT=$((PLATENUMBER+1))
-NEXTFOLDER="$(printf "p%0*d" 3 $PLATENEXT)"
-
+EXP_DIR=/eos/experiment/sndlhc/emulsionData/2022/emureco_CERN/RUN$RUN/$BRICKFOLDER/cells/$CELLFOLDER/$BRICKFOLDER
 
 echo "Set up SND environment"
 SNDBUILD_DIR=/afs/cern.ch/user/s/snd2cern/public/SNDBUILD/sw
@@ -27,34 +23,34 @@ export XROOTD_VMP=eospublic.cern.ch:/eos=/eos
 
 MAIN_DIR=$PWD
 cd $MAIN_DIR
-MY_DIR=${CELL}_${PLATENUMBER}/$BRICKFOLDER
-EXP_DIR=/eos/experiment/sndlhc/emulsionData/2022/emureco_CERN/RUN$RUN/$BRICKFOLDER/cells/$CELLFOLDER/$BRICKFOLDER
-mkdir -p -v ./$MY_DIR/$PLATEFOLDER
-mkdir -p -v ./$MY_DIR/$NEXTFOLDER
-mkdir -p -v ./$MY_DIR/AFF
-
-ln -s $EXP_DIR/$PLATEFOLDER/$BRICKID.$PLATENUMBER.0.0.raw.root ./$MY_DIR/$PLATEFOLDER
-ln -s $EXP_DIR/$NEXTFOLDER/$BRICKID.$PLATENEXT.0.0.raw.root ./$MY_DIR/$NEXTFOLDER
-ln -s $EXP_DIR/$PLATEFOLDER/$BRICKID.$PLATENUMBER.0.0.cp.root ./$MY_DIR/$PLATEFOLDER
-ln -s $EXP_DIR/$NEXTFOLDER/$BRICKID.$PLATENEXT.0.0.cp.root ./$MY_DIR/$NEXTFOLDER
+MY_DIR=${CELL}/$BRICKFOLDER
+for PLATENUMBER in $(seq 1 57); do
+    PLATEFOLDER="$(printf "p%0*d" 3 $PLATENUMBER)"
+    mkdir -p -v ./$MY_DIR/$PLATEFOLDER
+    ln -s $EXP_DIR/$PLATEFOLDER/$BRICKID.$PLATENUMBER.0.0.raw.root ./$MY_DIR/$PLATEFOLDER
+    ln -s $EXP_DIR/$PLATEFOLDER/$BRICKID.$PLATENUMBER.0.0.cp.root ./$MY_DIR/$PLATEFOLDER
+done
 ln -s $EXP_DIR/scanset.sh ./$MY_DIR
-ln -s $EXP_DIR/alignplate.sh ./$MY_DIR
+ln -s $EXP_DIR/align.sh ./$MY_DIR
 ln -s $EXP_DIR/align_*.rootrc ./$MY_DIR
 
-cd $MY_DIR
+tar -xzf AFF.tar.gz
+mv AFF ./$MY_DIR
 
-mv $MAIN_DIR/$BRICKID.$PLATENEXT.0.0_$BRICKID.$PLATENUMBER.0.0.aff.par ./AFF
+cd $MY_DIR
 
 echo "makescanset $BRICKID.0.0.0"
 source scanset.sh $BRICKID
 cp align_2.rootrc align.rootrc
-echo "align 2 $BRICKID.$PLATENUMBER.0.0"
-source alignplate.sh $BRICKID $PLATENUMBER
+echo "align 2 $BRICKID.0.0.0"
+source align_set.sh $BRICKID
 echo "makescanset $BRICKID.0.0.0"
 source scanset.sh $BRICKID
 cp align_3.rootrc align.rootrc
-echo "align 3 $BRICKID.$PLATENUMBER.0.0"
-source alignplate.sh $BRICKID $PLATENUMBER
+echo "align 3 $BRICKID.0.0.0"
+source align_set.sh $BRICKID
+echo "makescanset $BRICKID.0.0.0"
+source scanset.sh $BRICKID
 
-mv AFF/$BRICKID.$PLATENEXT.0.0_$BRICKID.$PLATENUMBER.0.0.al.root $MAIN_DIR/$BRICKID.$PLATENEXT.0.0_$BRICKID.$PLATENUMBER.$xcell.$ycell.al.root
-mv AFF/$BRICKID.$PLATENEXT.0.0_$BRICKID.$PLATENUMBER.0.0.aff.par $MAIN_DIR/$BRICKID.$PLATENEXT.0.0_$BRICKID.$PLATENUMBER.$xcell.$ycell.aff.par
+tar -czf AFF.tar.gz AFF/
+mv AFF.tar.gz $MAIN_DIR/AFF.$CELL.tar.gz
