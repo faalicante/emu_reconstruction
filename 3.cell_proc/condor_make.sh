8825602b3d47ddf57d5410d12ccdf8a99dace248
Fabio <alicantef@gmail.com>
2025-05-16 12:55:35 +0200

    make cells no env

--- 3.cell_proc/condor_make.sh
+++ 3.cell_proc/condor_make.sh
@@ -4,16 +4,11 @@ RUN=$2
 BRICKID=$3
 BRICKFOLDER="$(printf "b%0*d" 6 $BRICKID)"
 CELL=$4
-
-echo "Set up SND environment"
-SNDBUILD_DIR=/afs/cern.ch/user/s/snd2cern/public/SNDBUILD/sw
-source /cvmfs/sndlhc.cern.ch/SNDLHC-2023/Aug30/setUp.sh
-eval `alienv load -w $SNDBUILD_DIR --no-refresh sndsw/latest`
-echo "Loading FEDRA"
-source /afs/cern.ch/user/s/snd2cern/public/fedra/setup_new.sh	
+EXP_PRE=$5
+EXP_DIR=$EXP_PRE/RUN$RUN/$BRICKFOLDER/cells
 
 echo "go into reconstruction folder "
-cd /eos/experiment/sndlhc/emulsionData/2022/emureco_CERN/RUN$RUN/$BRICKFOLDER/cells
+cd $EXP_DIR
 
 echo "make volume $CELL"
 source make_volume.sh $BRICKID $CELL
