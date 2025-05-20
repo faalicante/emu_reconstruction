5c58fe807f9a639e4bc733a3985d20da6e0a509a
Fabio <alicantef@gmail.com>
2025-05-20 11:07:11 +0200

    third alignment correct

--- 4.align/condor_alp.sh
+++ 4.align/condor_alp.sh
@@ -57,8 +57,8 @@ source align.sh $BRICKID $PLATENUMBER
 echo "makescanset $BRICKID.0.0.0"
 source scanset.sh $BRICKID
 
-cp align_2.5.rootrc align.rootrc
-echo "align 2.5 $BRICKID.0.0.0"
+cp align_3.rootrc align.rootrc
+echo "align 3 $BRICKID.0.0.0"
 source align.sh $BRICKID $PLATENUMBER
 
 cp AFF/$BRICKID.$PLATENEXT.0.0_$BRICKID.$PLATENUMBER.0.0.aff.par $MAIN_DIR/$BRICKID.$PLATENEXT.0.0_$BRICKID.$PLATENUMBER.$xcell.$ycell.aff.par
