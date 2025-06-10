b90ac6279097a730fe10f3dfd4397e03ac4058df
Fabio <alicantef@gmail.com>
2025-06-10 14:42:38 +0200

    add mostag to new bricks

--- make_brick.sh
+++ make_brick.sh
@@ -12,6 +12,7 @@ cp $script_dir/create_link.sh $target_dir
 cp $script_dir/scanset.sh $target_dir
 cp $script_dir/1.mosaic/viewsideal.rootrc $target_dir
 cp $script_dir/1.mosaic/viewsideal.sh $target_dir
+cp $script_dir/2.tag/mostag.sh $target_dir
 cp $script_dir/2.tag/tagalignplate.sh $target_dir
 cp $script_dir/2.tag/tagalign.sh $target_dir
 cp $script_dir/2.tag/tagalign.rootrc $target_dir
