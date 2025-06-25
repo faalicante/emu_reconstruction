8160b573b5dc87759bb6c9b71af5a5a77123e0b9
Fabio <alicantef@gmail.com>
2025-06-25 12:19:18 +0200

    update scripts in maketemplate

--- 3.cell_proc/make_template.sh
+++ 3.cell_proc/make_template.sh
@@ -9,11 +9,11 @@ mkdir -p $template_dir
 cp -r AFF $template_dir
 cp viewsideal*.rootrc $template_dir
 cp $script_dir/scanset.sh $template_dir
-cp $script_dir/1.mosaic/viewsideal_set.sh $template_dir
+cp $script_dir/1.mosaic/viewsideal.sh $template_dir
 cp $script_dir/3.cell_proc/mos*.sh $template_dir
 cp $script_dir/3.cell_proc/make_volume.sh cells/
-cp $script_dir/4.align/align_set.sh $template_dir
-cp $script_dir/4.align/align_*.rootrc $template_dir
+cp $script_dir/4.align/alignplate.sh $template_dir
+cp $script_dir/4.align/align*.rootrc $template_dir
 cp $script_dir/5.tracking/tracking_all.sh $template_dir
 cp $script_dir/5.tracking/*.rootrc $template_dir
 cp $script_dir/6.vertexing/vertex.rootrc $template_dir
@@ -24,7 +24,3 @@ for PLATENUMBER in $(seq 1 57);do
     mkdir $template_dir/$PLATEFOLDER
     cp -a $PLATEFOLDER/$BRICKID.$PLATENUMBER.0.0.raw.root ./$template_dir/$PLATEFOLDER
 done
-cd $template_dir
-
-echo "makescanset $BRICKID.0.0.0"
-source scanset.sh $BRICKID
