71297cc8144da7ecd98218c39b33b10635aef2f1
Fabio <alicantef@gmail.com>
2025-06-26 16:04:56 +0200

    update filecheck

--- 3.cell_proc/fileproc_check.sh
+++ 3.cell_proc/fileproc_check.sh
@@ -2,24 +2,21 @@
 
 BRICKID=$1
 BRICKFOLDER="$(printf "b%0*d" 6 $BRICKID)"
-
 for CELL in $(seq 0 323); do
     xcell=$((CELL % 18 + 1))
     ycell=$((CELL / 18 + 1))
     folder=cell_${xcell}0_${ycell}0/$BRICKFOLDER
-
     for p in $(seq 1 57); do
         plate=$(printf "p%03d" $p)
         base="$folder/$plate/$BRICKFOLDER.$p.$xcell.$ycell"
         file_mos="${base}.mos.root"
         file_cp="${base}.cp.root"
         dest_base="$folder/$plate/$BRICKFOLDER.$p.0.0"
-
         if [ -f "$file_cp" ]; then
-            mv "$file_mos" "$dest_base.mos.root" 2>/dev/null
-            mv "$file_cp" "$dest_base.cp.root" 2>/dev/null
+            mv "$file_mos" "$dest_base.mos.root"
+            mv "$file_cp" "$dest_base.cp.root"
         else
-            echo "$p, $CELL -- $file_cp is missing"
+            echo "$p, $CELL"
         fi
     done
 done
--- 4.align/fileaff_check.sh
+++ 4.align/fileaff_check.sh
@@ -2,15 +2,18 @@
 
 BRICKID=$1
 BRICKFOLDER="$(printf "b%0*d" 6 $BRICKID)"
-CELL=$2
-xcell=$((CELL % 18 + 1))
-ycell=$((CELL / 18 + 1))
-folder=cell_${xcell}0_${ycell}0/$BRICKFOLDER
-file_aff=AFF.$CELL.tar.gz
-
-if [ -f "$folder/$file_aff" ]; then
-    cd "$folder"
-    tar -xzf "$file_aff"
-else
-    echo "$file_aff is missing" >> missing_files_$CELL.log
-fi
+for CELL in $(seq 1 323); do
+	xcell=$((CELL % 18 + 1))
+	ycell=$((CELL / 18 + 1))
+	folder=cell_${xcell}0_${ycell}0/$BRICKFOLDER/AFF
+	for p in $(seq 1 56); do
+		base=$BRICKID.$((p+1)).0.0_$BRICKID.$p
+		file_aff=$base.$xcell.$ycell.aff.par
+		if [ -f "$folder/$file_aff" ]; then
+   		    mv $folder/$file_aff $folder/$base.0.0.aff.par
+		    mv $folder/$base.$xcell.$ycell.al.root $folder/$base.0.0.al.root
+        else
+            echo "$p, $CELL"
+        fi
+    done
+done
