acb72896481a35cd80b1aa7527369b2fbf8e696c
Fabio <alicantef@gmail.com>
2025-06-26 16:10:56 +0200

    varie

--- 3.cell_proc/create_dat.py
+++ 3.cell_proc/create_dat.py
@@ -1,4 +1,4 @@
 with open("volumes.dat", "w+") as file:
-    for c1 in range(1, 58):
-        for c2 in range(0,324):
-            file.write(f"{c1},{c2}\n")
+    for plate in range(1, 58):
+        for cell in range(0,324):
+            file.write(f"{plate},{cell}\n")
--- make_brick.sh
+++ make_brick.sh
@@ -13,8 +13,10 @@ cp $script_dir/create_link.sh $target_dir
 cp $script_dir/scanset.sh $target_dir
 cp $script_dir/1.mosaic/viewsideal.rootrc $target_dir
 cp $script_dir/1.mosaic/viewsideal.sh $target_dir
+cp $script_dir/1.mosaic/check_mos.C $target_dir
 cp $script_dir/2.tag/mostag.sh $target_dir
 cp $script_dir/2.tag/tagalignplate.sh $target_dir
 cp $script_dir/2.tag/tagalign.sh $target_dir
 cp $script_dir/2.tag/tagalign.rootrc $target_dir
+cp $script_dir/2.tag/check_al.C $target_dir
 cd $target_dir
