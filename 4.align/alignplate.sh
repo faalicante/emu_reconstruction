478c2a2c7ba044e77a140970489ede3e8cae709a
Fabio <alicantef@gmail.com>
2026-04-29 14:14:47 +0200

    add alignR4

--- 4.align/alignplate.sh
+++ 4.align/alignplate.sh
@@ -1,3 +1,4 @@
 BRICKID=$1
 PLATENUMBER=$2
-emalign -A=$BRICKID.$((PLATENUMBER+1)).0.0 -B=$BRICKID.$((PLATENUMBER)).0.0 -new -v=1
+s=$3
+emalign -A=$BRICKID.$((PLATENUMBER+s)).0.0 -B=$BRICKID.$((PLATENUMBER)).0.0 -new -v=1
